0bf472bb3a9b3b9ec08cb7cd69368a69ef00c5a9
Win-Win-Tech <ravee.t@gmail.com>
2025-08-02 16:47:55 +0530

    first commit

---
+++ MySql_Connect.py
@@ -0,0 +1,26 @@
+import mysql.connector
+#print(mysql.connector.__version__)
+# Connect to the local MySQL database
+try:
+    conn = mysql.connector.connect(
+        host="193.203.184.6",
+        user="u547203012_alagarusr",
+        password="Yayaya@143",
+        database="u547203012_alagardb")
+    print(mysql.connector.__version__)
+    cursor = conn.cursor()
+    cursor.execute("SELECT Revenue FROM Balance_Sheet WHERE Revenue < 10000")
+    results = cursor.fetchall()
+    if results:
+        for row in results:
+            print(row)
+    else:
+        print("No matching rows found.")
+except mysql.connector.Error as err:
+    print("Database error:", err)
+except Exception as e:
+    print("Unexpected error:", e)
+finally:
+    if conn.is_connected():
+       print("Connected")
+    #  conn.close()
